4c2feb6fcdf21733c4c065fe845db36d73349cb8
Marco Zanotti <marco.zanotti@triboo.it>
2021-03-08 16:24:02 +0100

    cv repo

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CVitae
+
+My Curriculum Vitae is available here 
+
+My Resume is available here 
---
+++ rendering.r
@@ -0,0 +1,42 @@
+
+# CV ----------------------------------------------------------------------
+
+# * CV EN -----------------------------------------------------------------
+# Knit the HTML version
+rmarkdown::render("cv.rmd", output_file = "cv_resume/cv_en.html", params = list(pdf_mode = FALSE, lang = "en"))
+
+# Knit the PDF version
+tmp_html_cv_loc <- fs::file_temp(ext = ".html")
+rmarkdown::render("cv.rmd", output_file = tmp_html_cv_loc, params = list(pdf_mode = TRUE, lang = "en"))
+pagedown::chrome_print(input = tmp_html_cv_loc, output = "cv_resume/cv_en.pdf")
+
+# * CV IT -----------------------------------------------------------------
+# Knit the HTML version
+rmarkdown::render("cv.rmd", output_file = "cv_resume/cv_it.html", params = list(pdf_mode = FALSE, lang = "it"))
+
+# Knit the PDF version
+tmp_html_cv_loc <- fs::file_temp(ext = ".html")
+rmarkdown::render("cv.rmd", output_file = tmp_html_cv_loc, params = list(pdf_mode = TRUE, lang = "it"))
+pagedown::chrome_print(input = tmp_html_cv_loc, output = "cv_resume/cv_it.pdf")
+
+
+
+# Resume ------------------------------------------------------------------
+
+# * Resume EN -------------------------------------------------------------
+# Knit the HTML version
+rmarkdown::render("resume.rmd", output_file = "cv_resume/resume_en.html", params = list(pdf_mode = FALSE, lang = "en"))
+
+# Knit the PDF version
+tmp_html_resume_loc <- fs::file_temp(ext = ".html")
+rmarkdown::render("resume.rmd", output_file = tmp_html_resume_loc, params = list(pdf_mode = TRUE, lang = "en"))
+pagedown::chrome_print(input = tmp_html_resume_loc, output = "cv_resume/resume_en.pdf")
+
+# * Resume IT -------------------------------------------------------------
+# Knit the HTML version
+rmarkdown::render("resume.rmd", output_file = "cv_resume/resume_it.html", params = list(pdf_mode = FALSE, lang = "it"))
+
+# Knit the PDF version
+tmp_html_resume_loc <- fs::file_temp(ext = ".html")
+rmarkdown::render("resume.rmd", output_file = tmp_html_resume_loc, params = list(pdf_mode = TRUE, lang = "it"))
+pagedown::chrome_print(input = tmp_html_resume_loc, output = "cv_resume/resume_it.pdf")
